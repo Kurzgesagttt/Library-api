ffa8b6bb92fc98d7ae17febddf9526e5dec7c2d6
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-02-19 13:52:28 -0300

    ErroCampo tratamento

--- src/main/java/com/SpringJpa/demo/controller/common/GlobalExceptionHandler.java
+++ src/main/java/com/SpringJpa/demo/controller/common/GlobalExceptionHandler.java
@@ -1,15 +1,26 @@
 package com.SpringJpa.demo.controller.common;
 
+import com.SpringJpa.demo.controller.dto.ErroCampo;
 import com.SpringJpa.demo.controller.dto.ErroResposta;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public ErroResposta handleMethodNotValidException(MethodArgumentNotValidException ex){
-        return ErroResposta.respostaPadrao("Erro validacao");
+        List<FieldError> fieldErrors = ex.getFieldErrors();
+        List<ErroCampo> listaErros = fieldErrors.stream().map(fe -> new ErroCampo(fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ErroResposta(HttpStatus.UNPROCESSABLE_ENTITY.value(),"Erro de validacao",listaErros);
     }
 }
