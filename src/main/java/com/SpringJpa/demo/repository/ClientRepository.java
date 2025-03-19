0f10629de12493225856b693e4f7ec820a8492c0
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-03-19 14:41:30 -0300

    Client entity

--- src/main/java/com/SpringJpa/demo/repository/ClientRepository.java
+++ src/main/java/com/SpringJpa/demo/repository/ClientRepository.java
@@ -1,6 +1,9 @@
 package com.SpringJpa.demo.repository;
 
+import com.SpringJpa.demo.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends JpaRepository<> {
+import java.util.UUID;
+
+public interface ClientRepository extends JpaRepository<Client, UUID> {
 }
